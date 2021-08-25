3739342ca9a109eb808cf9a5e64d1331465d51c3
PhoenixPy-THT <89466578+PhoenixPy-THT@users.noreply.github.com>
2021-08-25 17:01:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = `!`;//your prefix for bot
+exports.Token = `ODgwMDU2NzYwMjUyMTIxMTE4.YSYuug.jxjxzW0w6gFWtMEB110Ap2O50bg`;//your token 
+exports.Color = `RED`;//color   of embed
---
+++ server.js
@@ -0,0 +1,94 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
+const { Prefix, Token, Color } = require("./config.js");
+const fetch = require("node-fetch");
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.db = require("quick.db");
+
+const activities_list = [
+    "!help", 
+    "Created by: ANKA TEAM",
+    "Alcatraz10#6666 - Phoenix.py#9429", 
+    "ANKA"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index], { type: 'PLAYING' });  //PLAYING: WATCHING: LISTENING: STREAMING.
+    }, 10000); // Runs this every 10 seconds.
+});
+
+client.on("message", async message => {
+  if (message.channel.type === "dm") return;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  if (message.content.match(new RegExp(`^<@!?${client.user.id}>`))) {
+    return message.channel.send(`what bro?`);
+  }
+});
+
+let modules = ["fun", "info", "moderation"];
+
+modules.forEach(function(module) {
+  fs.readdir(`./commands/${module}`, function(err, files) {
+    if (err)
+      return new Error(
+        "Missing Folder Of Commands! Example : Commands/<Folder>/<Command>.js"
+      );
+    files.forEach(function(file) {
+      if (!file.endsWith(".js")) return;
+      let command = require(`./commands/${module}/${file}`);
+      console.log(`${command.name} Command Has Been Loaded - ✅`);
+      if (command.name) client.commands.set(command.name, command);
+      if (command.aliases) {
+        command.aliases.forEach(alias =>
+          client.aliases.set(alias, command.name)
+        );
+      }
+      if (command.aliases.length === 0) command.aliases = null;
+    });
+  });
+});
+
+client.on("message", async message => {
+  if (message.channel.type === "dm") return;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  if (!message.content.startsWith(Prefix)) return;
+
+  const args = message.content
+    .slice(Prefix.length)
+    .trim()
+    .split(" ");
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  let command =
+    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+
+  if (!command) return;
+
+  if (command) {
+    command.run(client, message, args);
+  }
+  console.log(
+    `User : ${message.author.tag} (${message.author.id}) Server : ${message.guild.name} (${message.guild.id}) Command : ${command.name}`
+  );
+});
+
+setInterval(async () => {
+  await fetch('https://discord-modera-fun-bot.glitch.me').then(console.log('Pinged!'))
+}, 240000)
+
+client.login(Token);
